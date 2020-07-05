f9fcb7a943f2fcf8fb2ca3dc048fa0476eeb1eab
Roman Khlebnov <suppie.rk@gmail.com>
2020-07-05 05:40:45 +0300

    Removed Firebase for speed sake, updated Micronaut to 2.0.0

--- src/main/kotlin/ru/kugnn/microkonf/config/ConferencePropertiesLoader.kt
+++ src/main/kotlin/ru/kugnn/microkonf/config/ConferencePropertiesLoader.kt
@@ -1,13 +1,39 @@
 package ru.kugnn.microkonf.config
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import io.micronaut.context.annotation.Bean
 import io.micronaut.context.annotation.Factory
-import ru.kugnn.microkonf.persistence.FirestoreRepository
 
 @Factory
-class ConferencePropertiesLoader(
-        private val firestoreRepository: FirestoreRepository
-) {
+class ConferencePropertiesLoader {
+    private val yamlMapper: ObjectMapper by lazy { ObjectMapper(YAMLFactory()) }
+
     @Bean
-    fun conferenceProperties(): ConferenceProperties = firestoreRepository.readOrInsert()
+    fun conferenceProperties(): ConferenceProperties = ConferenceProperties(
+            constants = "blocks/constants.yaml".readPropertyFromFile(),
+            blocks = "blocks/page-blocks-order.yaml".readPropertyFromFile(),
+            conference = "blocks/index/conference.yaml".readPropertyFromFile(),
+            gallery = "blocks/index/gallery.yaml".readPropertyFromFile(),
+            organizers = "blocks/index/organizers.yaml".readPropertyFromFile(),
+            partners = "blocks/index/partners.yaml".readPropertyFromFile(),
+            resources = "blocks/index/resources.yaml".readPropertyFromFile(),
+            statistics = "blocks/index/statistics.yaml".readPropertyFromFile(),
+            tickets = "blocks/index/tickets.yaml".readPropertyFromFile(),
+            venue = "blocks/index/venue.yaml".readPropertyFromFile(),
+            schedule = "blocks/schedule/schedule.yaml".readPropertyFromFile(),
+            commonSessions = "blocks/sessions/common-sessions.yaml".readPropertyFromFile(),
+            speakerSessions = "blocks/sessions/speaker-sessions.yaml".readPropertyFromFile(),
+            speakers = "blocks/speakers/speakers.yaml".readPropertyFromFile(),
+            teams = "blocks/team/team.yaml".readPropertyFromFile()
+    )
+
+    private inline fun <reified T> String.readPropertyFromFile(): T {
+        require(!this.isBlank()) { "Resource name is not defined" }
+
+        return ClassLoader.getSystemClassLoader().getResource(this)?.run {
+            yamlMapper.readValue(this, object : TypeReference<T>() {})
+        } ?: error("Cannot load $this resource")
+    }
 }
